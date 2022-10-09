e81c61b6672118951f61addaa08361943afa7115
emoore8902 <e.moore8902@gmail.com>
2022-10-08 23:36:08 -0700

    Began styling

--- src/components/location.js
+++ src/components/location.js
@@ -7,13 +7,18 @@ export default function Location(props) {
             src = {props.imageUrl}
             className = "locations--image"
             />
-            <div className = "locations--maps">
-                <span>{props.location} </span>
-                <span>{props.googleMapsUrl}</span>
+            <div className = "locations--content">
+                <div className = "locations--maps">
+                    <span>
+                        <img src = "https://gcdnb.pbrd.co/images/DmtN5mzZWtm2.png?o=1" className = "location--symbol"/>
+                        {props.location} 
+                    </span>
+                    <span className = "locations--maps--link"><a href = "{props.googleMapsUrl}">View on Google Maps.</a></span>
+                </div>
+                <h2 className = "locations--title">{props.title}</h2>
+                <p className = "locations--date">{props.startDate} - {props.endDate}</p>
+                <p className = "locations--description">{props.description}</p>
             </div>
-            <h2 className = "locations--title">{props.title}</h2>
-            <p className = "locations--date">{props.startDate} - {props.endDate}</p>
-            <p className = "locations--description">{props.description}</p>
         </div>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src = "https://png.pngtree.com/png-vector/20210309/ourmid/pngtree-earth-png-image_3021401.jpg" className = "nav--logo"></img>
+            <img src = "https://gcdnb.pbrd.co/images/vJDZdxSIFBI5.png?o=1" className = "nav--logo"></img>
             <h1>my travel journal.</h1>
         </nav>
     )
